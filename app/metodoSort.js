ca7e061dc9bdbd5a85bdf6f1d687fafe5227bce1
henriqueCostaSI <henrique.mcosta@hotmail.com>
2022-10-06 22:16:13 -0300

    commit

---
+++ app/metodoSort.js
@@ -0,0 +1,10 @@
+let btnOdenarPorPreco = document.getElementById('btnOrdenarPorPreco');
+
+btnOdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco);
+
+function ordenarLivrosPorPreco() {
+
+    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco);
+
+    exibirOsLivros(livrosOrdenados);
+}
